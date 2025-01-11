7a837717bbef0457c271cfcc80bbbdbfc3573de3
eric-j-owen <ejowen+github@proton.me>
2025-01-11 14:34:12 -0500

    adds logging

--- functions/api/submit.ts
+++ functions/api/submit.ts
@@ -6,6 +6,7 @@ export async function onRequestPost(context: Ctx) {
   try {
     const input = await context.request.formData();
     const pretty = JSON.stringify([...input], null, 2);
+    console.log(pretty);
     return new Response(pretty, {
       headers: {
         "Content-Type": "application/json;charset=utf-8",
