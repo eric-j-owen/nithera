0f8f1543d93593b63ff068d73cd815d66dece256
eric-j-owen <ejowen+github@proton.me>
2025-01-12 15:08:42 -0500

    changes function signature

--- functions/api/submit.ts
+++ functions/api/submit.ts
@@ -1,21 +1,33 @@
 import { Resend } from "resend";
-import type { EventContext } from "@cloudflare/workers-types";
 
-interface Env {
+interface EnvType {
   RESEND_API_KEY: string;
 }
 
 export async function onRequestPost({
   request,
   env,
-}: EventContext<Env, string, unknown>) {
+}: {
+  request: Request;
+  env: EnvType;
+}): Promise<Response> {
   try {
+    // get data
     const input = await request.formData();
     const fullName = input.get("fullname");
     const email = input.get("email");
     const phoneNumber = input.get("phonenumber");
     const message = input.get("message");
 
+    // validation
+    if (!fullName) {
+      return new Response("Missing name", { status: 400 });
+    }
+
+    if (!message) {
+      return new Response("Missing name", { status: 400 });
+    }
+    // send email
     const resend = new Resend(env.RESEND_API_KEY);
     const emailPayload = await resend.emails.send({
       cc: [],
