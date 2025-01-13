21bdcd60bac7dc5792648a85a461d03cdcf592b8
eric-j-owen <ejowen+github@proton.me>
2025-01-12 20:35:42 -0500

    cleans up submit endpoint, adds validation and logging and csrf protections

--- functions/api/submit.ts
+++ functions/api/submit.ts
@@ -12,7 +12,18 @@ export async function onRequestPost({
   env: EnvType;
 }): Promise<Response> {
   try {
-    // get data
+    if (!env.RESEND_API_KEY) {
+      return new Response("Missing env variable", { status: 500 });
+    }
+
+    //csrf protection
+    const allowedOrigins = ["http://localhost:8788"];
+    const origin = request.headers.get("Origin");
+    if (!origin || !allowedOrigins.includes(origin)) {
+      return new Response("CSRF validation failed", { status: 403 });
+    }
+
+    // get form data
     const input = await request.formData();
     const fullName = input.get("fullname");
     const email = input.get("email");
@@ -20,13 +31,18 @@ export async function onRequestPost({
     const message = input.get("message");
 
     // validation
-    if (!fullName) {
-      return new Response("Missing name", { status: 400 });
+    if (!fullName || typeof fullName !== "string" || fullName.length < 1) {
+      return new Response("Invalid name", { status: 400 });
     }
 
-    if (!message) {
-      return new Response("Missing name", { status: 400 });
+    if (!message || typeof message !== "string" || message.length < 1) {
+      return new Response("Invalid message", { status: 400 });
     }
+
+    if (!email || typeof email !== "string" || email.length < 1) {
+      return new Response("Invalid email", { status: 400 });
+    }
+
     // send email
     const resend = new Resend(env.RESEND_API_KEY);
     const emailPayload = await resend.emails.send({
@@ -44,6 +60,7 @@ export async function onRequestPost({
 
     return Response.json(emailPayload);
   } catch (err) {
+    console.error("Error:", err);
     return new Response("Error parsing JSON content", { status: 400 });
   }
 }
