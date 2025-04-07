a4f4a2ce5cceaa3a4c3c3757023c83f002263de3
eric-j-owen <ejowen+github@proton.me>
2025-04-07 18:31:57 -0400

    adds sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     svg: true,
   },
+  site: "https://neuroimpacttx.com",
+  integrations: [sitemap()],
 });
